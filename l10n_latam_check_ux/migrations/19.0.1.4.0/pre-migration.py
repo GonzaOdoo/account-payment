fbbcbd446311c70d74a07932801c1ef3e8138168
GonzaOdoo <zelayagonzalo33@gmail.com>
2026-06-12 21:53:32 -0300

    Updated migration script

--- l10n_latam_check_ux/migrations/19.0.1.4.0/pre-migration.py
+++ l10n_latam_check_ux/migrations/19.0.1.4.0/pre-migration.py
@@ -1,2 +1,12 @@
+import logging
+
+_logger = logging.getLogger(__name__)
 def migrate(cr, version):
     cr.execute("COMMENT ON INDEX l10n_latam_check_unique IS 'index marked to upgrade'")
+    cr.execute("""
+        UPDATE l10n_latam_check
+           SET payment_date = create_date::date
+         WHERE id = 1400
+           AND payment_date IS NULL
+    """)
+    _logger.warning("Set payment_date to create_date for l10n_latam_check with id 1400")
